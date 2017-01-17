5b424da2a8051e7aed79df942fb81f3983798066
Mark Muchane <muchanem@gmail.com>
2017-01-17 14:12:07 -0500

    save

--- src/variables_constants_types/main.go
+++ src/variables_constants_types/main.go
@@ -1,5 +1,6 @@
 package main
 
+//
 import (
 	"fmt"
 	"strings"
@@ -10,14 +11,15 @@ func main() {
 	fmt.Printf("string1: %v:%T\n", string1, string1)
 	var string2 = "explicitly typed string"
 	fmt.Printf("string2: %v:%T\n", string2, string2)
-
+	// Puts string upper case
 	fmt.Println(strings.ToUpper(string1))
+	// Captializes every first letter
 	fmt.Println(strings.Title(string2))
 	lowerValue := "hello"
 	upperValue := "HELLO"
+	// == to see if they afre equal
 	fmt.Println("Same?", (lowerValue == upperValue))
 	fmt.Println("Equal when non case sensetive?", strings.EqualFold(lowerValue, upperValue))
-	fmt.Println(strings.EqualFold(lowerValue, upperValue))
 	fmt.Println("Contains exp?", strings.Contains(string1, "exp"))
 	fmt.Println("Contains exp?", strings.Contains(string2, "exp"))
 
