0df2891152db216bada3d71c410e758208d0044f
Mark Muchane <muchanem@gmail.com>
2017-01-17 07:50:08 -0500

    save

--- src/variables_constants_types/main.go
+++ src/variables_constants_types/main.go
@@ -1,13 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	//Explicitly
-	var anInteger = 13
-	// Implicitly
-	aString := "Mark"
-	//Explicitly
-	const Integer = 13
-	// Implicitly
-	const string1 = "Mark"
-	println(anInteger, aString, Integer, string1)
+	str1 := "An implicitly typed string"
+	Printf("str1: %v:%T\n", str1, str1)
+	var str2 = "An explicitly typed string"
+	Printf("str2: %v:%T\n", str2, str2)
+
+	println(strings.ToUpper(str1))
+	println(strings.Title(str2))
+
+	lValue := "hello"
+	uValue := "HELLO"
+	fmt.Println("Equal?", (lValue == uValue))
+	fmt.Println("Equal Non-Case-Sensitive?", strings.EqualFold(lValue, uValue))
+
+	fmt.Println("Contains exp?", strings.Contains(str1, "exp"))
+	fmt.Println("Contains exp?", strings.Contains(str2, "exp"))
 }
