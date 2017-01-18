c9a1f9250936397b73d74c3bcb7fbd4177c0cb47
Mark Muchane <muchanem@gmail.com>
2017-01-18 06:43:21 -0500

    save

--- src/basic_math/main.go
+++ src/basic_math/main.go
@@ -1,7 +1,12 @@
 package main
 
-func main() {
-	int1, int2, int3 := 13, 14, 15
-	intsum := int1 + int2 + int3
+import "fmt"
 
+func main() {
+	// You can assiagn variables of the same type in this way
+	int1, int2, int3 := 13, 14, 15
+	// Just adding
+	intsum := int1 + int2 + int3
+	// Basic Println
+	fmt.Println("Sum:", intsum)
 }
