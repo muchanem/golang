51ea98d9d3908dcfeb6653cc869e9f3f8ebd7450
Mark Muchane <muchanem@gmail.com>
2017-01-23 06:07:18 -0500

    save

--- src/date_time/main.go
+++ src/date_time/main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	//seting time (0s for seconds, nanoseconds) utc for location
+	//seting time (0s for seconds, nanoseconds) utc for GMT time, local for local time
 	time := time.Date(2017, time.January, 22, 9, 51, 0, 0, time.Local)
 	fmt.Printf("When I wrote this program:%s\n", time)
+
+	currrentTime := time.Now()
+	fmt.Println(currrent_time)
 }
