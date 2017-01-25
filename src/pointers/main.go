e2d00f4cad049e0322d74f8a71e6212c12dffa6c
Mark Muchane <muchanem@gmail.com>
2017-01-25 12:58:37 -0500

    save

--- src/pointers/main.go
+++ src/pointers/main.go
@@ -25,11 +25,17 @@ func main() {
 	} else {
 		fmt.Println("Pointer is nil")
 	}
+	// defining another value
 	value2 := 13.2
+	// setting pointer2 to that value
 	pointer2 := &value2
+	// printing the value of what the pointer is pointing at
 	fmt.Println("Value of pointer2", *pointer2)
+	// dividing the value that the pointer is pointing at by 2
 	*pointer2 = *pointer2 / 2
+	// printing the value of what the pointer is pointing at
 	fmt.Println("Pointer2:", *pointer2)
+	// showing that my divide by two operation actually effected the pointer
 	fmt.Println("Value:", value2)
 
 }
