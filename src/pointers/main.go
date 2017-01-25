89c26033727887469219dc7f6aea393138114652
Mark Muchane <muchanem@gmail.com>
2017-01-25 12:37:17 -0500

    save

--- src/pointers/main.go
+++ src/pointers/main.go
@@ -4,24 +4,24 @@ import "fmt"
 
 func main() {
 	// delcaring a pointer that points at a int value
-	var pointer *int
+	var pointer1 *int
 
 	// checking is p  = nil (it does)
-	if pointer != nil {
+	if pointer1 != nil {
 		// use asterisek to show it is pointer
-		fmt.Println("The value of 'pointer' is:", *pointer)
+		fmt.Println("The value of 'pointer' is:", *pointer1)
 		// no asterisek for adress
-		fmt.Println("The adress is:", pointer)
+		fmt.Println("The adress is:", pointer1)
 	} else {
 		fmt.Println("Pointer is nil")
 	}
 	value1 := 13
-	pointer = &value1
-	if pointer != nil {
+	pointer1 = &value1
+	if pointer1 != nil {
 		// use asterisek to show it is pointer
-		fmt.Println("The value of pointer is:", *pointer)
+		fmt.Println("The value of pointer is:", *pointer1)
 		// no asterisek for adress
-		fmt.Println("The adress is:", pointer)
+		fmt.Println("The adress is:", pointer1)
 	} else {
 		fmt.Println("Pointer is nil")
 	}
