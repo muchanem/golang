12c369e4878bda146473d84f8b7ffb20638909fd
Mark Muchane <muchanem@gmail.com>
2017-01-25 06:10:40 -0500

    save

--- src/pointers/main.go
+++ src/pointers/main.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	// delcaring a pointer that points at a int value
 	var p *int
+
+	// checking is p  = nil (it does)
+	if p != nil {
+		// use asterisek to show it is pointer
+		fmt.Println("The value of p is:", *p)
+	} else {
+		fmt.Println("P is nil")
+	}
 }
