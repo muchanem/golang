e6083ad373991cc11ecf6e8e510553e2dcc371c9
Mark Muchane <muchanem@gmail.com>
2017-01-31 06:26:48 -0500

    save

--- src/arrays_slices/main.go
+++ src/arrays_slices/main.go
@@ -14,4 +14,8 @@ func main() {
 	fmt.Println(colours)
 	// use this method to get to single item in array
 	fmt.Println(colours[2])
+	// declaring and setting array in one line
+	var numbers = [5]int{5, 4, 3, 2, 1}
+	// printing it
+	fmt.Println(numbers)
 }
