f252931b1e07e2484e2902300bd3b5514d8d3e40
Mark Muchane <muchanem@gmail.com>
2017-01-31 06:20:41 -0500

    save

--- src/arrays_slices/main.go
+++ src/arrays_slices/main.go
@@ -1,9 +1,14 @@
 package main
+
 import (
-  "fmt"
+	"fmt"
 )
+
 func main() {
-  // setting array with string called colours
-  var colours [3]string
-  colours[]
+	// setting array with string called colours
+	var colours [3]string
+	colours[0] = "Red"
+	colours[1] = "Orange"
+	colours[2] = "Yellow"
+	fmt.Println(colours)
 }
