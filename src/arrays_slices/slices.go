f1b0b5afe6c724568874c64dcf9ff475793d198d
Mark Muchane <muchanem@gmail.com>
2017-02-02 06:24:30 -0500

    save

--- src/arrays_slices/slices.go
+++ src/arrays_slices/slices.go
@@ -12,10 +12,10 @@ func main() {
 	// adding another colour using append
 	colours = append(colours, "Blue")
 	fmt.Println(colours)
-	// 1 is the second value, and we ar cutting everything below that value off so it is removing red
+	// saying give me back all items ecxept the first one (you dont need length of colurs, that is already the default)
 	colours = append(colours[1:len(colours)])
 	fmt.Println(colours)
-	// defualt is len of colours, just proving
-	colours = append(colours[1:])
+	// removing last item
+	colours = append(colours[:len(colours)-1])
 	fmt.Println(colours)
 }
