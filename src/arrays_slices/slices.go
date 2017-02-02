9f979b151c75323643a030179292984af5ad4159
Mark Muchane <muchanem@gmail.com>
2017-02-02 06:18:22 -0500

    save

--- src/arrays_slices/slices.go
+++ src/arrays_slices/slices.go
@@ -7,6 +7,9 @@ import (
 func main() {
 	// declaring and setting a sclice
 
-	var colours = []string{"Red", "Green", "Blue"}
+	var colours = []string{"Red", "Orange", "Green"}
+	fmt.Println(colours)
+	// adding another colour using append
+	colours = append(colours, "Blue")
 	fmt.Println(colours)
 }
