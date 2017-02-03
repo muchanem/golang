1d9a5608dfb1f32074e4a3f51c5e9874705b2e1a
Mark Muchane <muchanem@gmail.com>
2017-02-03 06:07:57 -0500

    save

--- src/arrays_slices/slices.go
+++ src/arrays_slices/slices.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -21,11 +22,11 @@ func main() {
 	// implicitly declaring slice and using make function to set type to int, intital lenth to 5 and the most values to 5
 	numbers := make([]int, 5, 5)
 	// seting the values
-	numbers[0] = 1
-	numbers[1] = 2
+	numbers[0] = 11
+	numbers[1] = 7
 	numbers[2] = 5
-	numbers[3] = 7
-	numbers[4] = 11
+	numbers[3] = 2
+	numbers[4] = 1
 	fmt.Println(numbers)
 	// adding number to slice
 	numbers = append(numbers, 13)
@@ -33,5 +34,8 @@ func main() {
 	fmt.Println(numbers)
 	// since 13 exaeded the cap is doubled
 	fmt.Println(cap(numbers))
+	//putting in numerical order
+	sort.Ints(numbers)
+	fmt.Println(numbers)
 
 }
