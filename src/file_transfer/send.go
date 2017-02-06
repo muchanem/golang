8f65b751f72b67f579513536bbad8062488eab18
Mark Muchane <muchanem@gmail.com>
2017-02-06 07:53:11 -0500

    save

--- src/file_transfer/send.go
+++ src/file_transfer/send.go
@@ -1,35 +1,39 @@
 package main
 
 import (
-  "fmt"
-  "io"
-  "net"
-  "os"
-  "strconv"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
 )
+
 // defining how big the packets will be
-const BufferSize = 1024
+const BUFFERSIZE = 1024
 
 func main() {
-  // defining two functions, one listens of tcp, the other port 27001
-  server, err := net.Listen("tcp", "localhost:27001")
-  // if there is something on 27001, it throughs an error
-  if err != nil {
+	// defining two functions, one listens of tcp, the other port 27001
+	server, err := net.Listen("tcp", "localhost:27001")
+	// if there is something on 27001, it throughs an error
+	if err != nil {
 		fmt.Println("Error listetning: ", err)
 		os.Exit(1)
 	}
-  // if the search goes out of scope the operation is shut down
-  defer server.Close()
-  // this loop wait until someone connects, then sets up a go routine to send the file to the client then starts the loop again
-  for {
-    connection, err := server.Accept()
-    if err != nil {
-      fmt.Println("Error: ", err)
-      os.Exit(1)
-    }
-    fmt.Println("Reciever connected")
-    go sendFileToClient(connection)
+	// if the search goes out of scope the operation is shut down
+	defer server.Close()
+	// this loop wait until someone connects, then sets up a go routine to send the file to the client then starts the loop again
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+		fmt.Println("Reciever connected")
+		go sendFileToClient(connection)
+
+	}
 }
+
 // this is the juicy part (the part "go" called)
 func sendFileToClient(connection net.Conn) {
 	fmt.Println("A reciever has connected!")
@@ -61,4 +65,14 @@ func sendFileToClient(connection net.Conn) {
 	fmt.Println("File has been sent, closing connection!")
 	return
 }
+func fillString(retunString string, toLength int) string {
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
 }
