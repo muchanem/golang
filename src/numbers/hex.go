afa8fcae79e58fbf5df7229ac0eee8d7c85cec79
Mark Muchane <muchanem@gmail.com>
2017-04-01 16:04:29 -0400

    save

--- src/numbers/hex.go
+++ src/numbers/hex.go
@@ -7,6 +7,11 @@ import (
 	"fmt"
 )
 
+// using prinf to print with formating
+// %d for decimal gets applied to first number
+// %b for binary gets applied to second number
+// %x prints in hex with lowercase letters
+// \n an escape character for newline (println already does this hence ln)
 func main() {
-	fmt.Printf("%d - %b - %x", 13, 13, 13)
+	fmt.Printf("%d %b %x", 13, 13, 13)
 }
