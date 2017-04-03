9d958057cd10d7b5f7753b98546906cdfdde45a1
Mark Muchane <muchanem@gmail.com>
2017-04-03 08:52:33 -0400

    save

--- src/numbers/loop.go
+++ src/numbers/loop.go
@@ -1,8 +1,24 @@
 package main
 
+import "fmt"
+
 /*
   Printing numbers in using a loop in different formats
 */
 
 func main() {
+	//
+	for i := 0; i < 200; i++ {
+		/*
+		   using prinf to print with formating
+		   %d for decimal gets applied to first number
+		   %b for binary gets applied to second number
+		   %#x prints in hex with lowercase letters with a zero x in front
+		   %x prints in hex lowercase letters
+		   you can change to capital letters by doing %X
+		   \n an escape character for newline (println already does this hence ln)
+		*/
+		fmt.Printf("%d \t %b \t %#x - %x \n", 13, 13, 13, 13)
+	}
+
 }
