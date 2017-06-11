a8a53b0ed6754fde5a1900d35edbe39ab6cd7af9
Mark Muchane <muchanem@gmail.com>
2017-06-10 21:08:59 -0400

    Create utf8_loop.go

--- cmd/format/utf8_loop.go
+++ cmd/format/utf8_loop.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-
+// same as number loop just using %q which prints the value in utf8
 	for i := 0; i < 200; i++ {
 		fmt.Printf("%d \t %b \t %x \t %q \n ", i, i, i, i)
 
